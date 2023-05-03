60865b39e681c82c5e1d8f6f1b21afee94cae09e
Aniketh Janardhan Reddy <anikethjr@gmail.com>
2023-05-03 15:40:20 -0700

    Update deps

--- setup.py
+++ setup.py
@@ -8,5 +8,5 @@ setuptools.setup(
     install_requires=["numpy", "pandas", "argparse", "wandb", "h5py", "tqdm", "scipy", "scikit-learn", "matplotlib", "seaborn",\
                       "torch", "torchvision", "torchmtl", "lightning", \
                       "kipoiseq", "pyfaidx", "joblib", "fastsk", "editdistance", "fastdist", "numba", \
-                      "transformers", "tensorly", "tensorly-torch", "odfpy"]
+                      "transformers", "tensorly", "tensorly-torch", "odfpy", "biopython"]
 )
