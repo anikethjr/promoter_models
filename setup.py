46f041d7fc68be7c453304fcd37cd1eadf66b6d6
Aniketh Janardhan Reddy <anikethjr@gmail.com>
2023-05-02 18:25:19 -0700

    Update dependencies

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setuptools.setup(
     include_package_data=True,
     packages=setuptools.find_packages(),
     install_requires=["numpy", "pandas", "argparse", "wandb", "h5py", "tqdm", "scipy", "scikit-learn", "matplotlib", "seaborn",\
-                      "torch", "torchvision", "torchmetrics", "torchmtl", "pytorch-lightning", \
+                      "torch==2", "torchvision", "torchmetrics", "torchmtl", "pytorch-lightning", \
                       "kipoiseq", "pyfaidx", "joblib", "fastsk", "editdistance", "fastdist", "numba", \
                       "transformers", "tensorly", "tensorly-torch", "odfpy"]
 )
