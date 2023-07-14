f6af0d254904261da43460c48968fc6d4bc3b3ef
Aniketh Janardhan Reddy <anikethjr@gmail.com>
2023-07-13 22:16:59 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setuptools.setup(
     include_package_data=True,
     packages=setuptools.find_packages(),
     install_requires=["numpy", "pandas", "argparse", "wandb", "h5py", "tqdm", "scipy", "scikit-learn", "matplotlib", "seaborn",\
-                      "torch", "torchvision", "torchaudio==1.13.1", "torchdata==0.5.1", "torchtext==0.14", "torchmtl", "lightning", \
+                      "torch", "torchvision", "torchaudio==0.13.1", "torchdata==0.5.1", "torchtext==0.14", "torchmtl", "lightning", \
                       "kipoiseq", "pyfaidx", "joblib", "fastsk", "editdistance", "fastdist", "numba", \
                       "transformers", "tensorly", "tensorly-torch", "odfpy", "biopython", "einsum", "rotary-embedding-torch"]
 )
