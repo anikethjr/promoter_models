afeceafaa127f00a7ae9d59c60311845a598e5d9
Aniketh Janardhan Reddy <anikethjr@gmail.com>
2023-09-22 15:51:37 -0700

    fix

--- promoter_modelling/backbone_modules.py
+++ promoter_modelling/backbone_modules.py
@@ -273,6 +273,8 @@ class MPRAnn(nn.Module):
         self.dropout3 = nn.Dropout(0.3)
         self.fc2 = nn.Linear(300, 200)
 
+        self.embed_dims = None
+
     def forward(self, seq):
         seq = seq.permute(0, 2, 1)
         seq = self.conv1(seq)
@@ -297,6 +299,9 @@ class MPRAnn(nn.Module):
         seq = self.dropout3(seq)
         seq = self.fc2(seq)
         seq = F.sigmoid(seq)
+
+        if self.embed_dims is None:
+            self.embed_dims = seq.shape[1]
 
         return seq
 
