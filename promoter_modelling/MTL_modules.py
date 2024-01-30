81494637909cb09076e68c122ce34909b8e9ff06
Aniketh Janardhan Reddy <anikethjr@gmail.com>
2024-01-30 14:25:57 -0800

    bug fix

--- promoter_modelling/MTL_modules.py
+++ promoter_modelling/MTL_modules.py
@@ -20,10 +20,8 @@ from lightning.pytorch.utilities.combined_loader import CombinedLoader
 
 import torchmtl
 
-import boda
-from boda.model.custom_layers import Conv1dNorm, LinearNorm, GroupedLinear, RepeatLayer, BranchedLinear
-
 from promoter_modelling.backbone_modules import *
+from boda.model.custom_layers import Conv1dNorm, LinearNorm, GroupedLinear, RepeatLayer, BranchedLinear
 
 np.random.seed(97)
 torch.manual_seed(97)
